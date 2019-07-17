270718cac7b2d4c8bf6797808b8fa209cc8a37a3
OzgulBerkan <20544048+OzgulBerkan@users.noreply.github.com>
2019-07-17 23:30:26 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    setTimeout(function(){
+    $("#owl-demo").owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+        responsive:false
+    });
+},1000)
+});
+
+
+$('button').click(function () {
+    $(this).toggleClass('expanded').siblings('div').slideToggle();
+});
